515446dbed9acc043497254631c2aef670a3dfb9
Shuxin-Wang <81904920+A-BigTree@users.noreply.github.com>
2022-11-19 15:05:03 +0800

    del pretreatment

--- pretreatment/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-# @author  : Shuxin_Wang
-# @email   : 213202122@seu.edu.cn
-# @time    : 2022/9/12 
-# @function: the script is used to do something.
-# @version : V1.0 
-#
-
-from .pretreatment import JsonDataManage, CSVPretreatment, PretreatmentManage
